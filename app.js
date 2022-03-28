200b001ce67268c664208c4f620ce57b1861a9a3
John Kuefler <john@dev-squared.com>
2022-03-28 15:49:09 -0500

    fixes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var homePageRouter = require('./routes/dashboard');
 var loginRouter = require('./routes/login');
 var patientRouter = require('./routes/patient');
@@ -25,11 +24,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dashboard', homePageRouter)
 app.use('/login', loginRouter);
-app.use('./patient', patientRouter);
-app.use('./AccountDetails', AccountDetailsRouter);
+app.use('/patient', patientRouter);
+app.use('/AccountDetails', AccountDetailsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/login.js
+++ routes/login.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-router.get('/', function(req, res, next) {
-    axios.get('https://jsonplaceholder.typicode.com/login/')
-    .then(function(response) {
-      console.log(response.data);
-      res.render('login', {login: response.data});
-    })
-    
-    });
+router.get('/', function (req, res,) {
+  res.render('login');
+});
 
-    module.exports = router;
+module.exports = router;
