af58d3f5aaec8840a1860446af3c545d1eef8124
praterkyle42 <kprater@gus.pittstate.edu>
2022-04-11 00:13:13 -0500

    Added database and CRUD matrix for Patients

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,5 +1,16 @@
+const Patient = require("../models/patientSchema");
+
 exports.get_patient = function(req,res) {
-    res.render('Patient/index');
+    
+    Patient.find({}, function(err, data) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            res.render('Patient/index', {patients: data} );
+        }
+
+    })
 }
 
 exports.get_addNewPatient = function(req,res) {
@@ -7,39 +18,87 @@ exports.get_addNewPatient = function(req,res) {
 }
 
 exports.post_newPatient = function(req,res) {
-    res.render('Patient/addNewPatient')
-    let patient = {
-        creatorID: req.body.creatorID, 
-        firstName: req.body.firstName, 
-        lastName: req.body.lastName, 
-        birthday: req.body.birthday, 
-        zipCode: req.body.zipCode, 
-        state: req.body.state, 
-        phone: req.body.phone, 
-        createDate: req.body.createDate, 
-        insuranceType: req.body.insuranceType, 
-        testType: req.body.testType, 
-        doctorService: req.body.doctorService, 
-        lab: req.body.lab, 
-        status: req.body.status
+    let enabled = false; 
+    if(req.body.enabled == 'on') {
+        enabled = true;
+    }
     
-     }
-     res.redirect('Patient/index')
+    let newPatient = new Patient({
+        CreatorId: req.body.creatorID, 
+        CreatorName: req.body.creatorName, 
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName, 
+        Birthdate: req.body.birthDate,
+        Zipcode: req.body.zipCode, 
+        State: req.body.inputState, 
+        PhoneNumber: req.body.phone, 
+        CreateDate: req.body.birthDate,
+        InsuranceType: req.body.insurance, 
+        TestType: req.body.testType, 
+        DoctorService: req.body.service, 
+        LabName: req.body.lab, 
+        SampleStatus: req.body.status
+    })
+
+    newPatient.save(function(err) {
+        if(err) {
+            console.log(err)
+        }
+        else {
+            console.log("Patient Saved!")
+        }
+    })
+
+    res.render('Patient/thankyou')
 }
 
-let patient = {
-    creatorID: req.body.creatorID, 
-    firstName: req.body.firstName, 
-    lastName: req.body.lastName, 
-    birthday: req.body.birthday, 
-    zipCode: req.body.zipCode, 
-    state: req.body.state, 
-    phone: req.body.phone, 
-    createDate: req.body.createDate, 
-    insuranceType: req.body.insuranceType, 
-    testType: req.body.testType, 
-    doctorService: req.body.doctorService, 
-    lab: req.body.lab, 
-    status: req.body.status
+exports.get_updatePatient = function(req, res) {
+    Patient.findOne({ _id: req.query.id }, function (err, patient) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(patient);
+            res.render('Patient/updatePatient', { data: patient });
+        }
+    });
+}
 
- }
+exports.post_updatePatient = function (req, res) {
+
+    const updateData = {
+        CreatorId: req.body.creatorID, 
+        CreatorName: req.body.creatorName, 
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName, 
+        Birthdate: req.body.birthDate,
+        Zipcode: req.body.zipCode, 
+        State: req.body.inputState, 
+        PhoneNumber: req.body.phone, 
+        CreateDate: req.body.createDate,
+        InsuranceType: req.body.insurance, 
+        TestType: req.body.testType, 
+        DoctorService: req.body.service, 
+        LabName: req.body.lab, 
+        SampleStatus: req.body.status
+    };
+
+    Patient.findOneAndUpdate({ _id: req.body.id }, updateData, function (err, data) {
+        if (err) {
+            // handle error
+            console.log(err);
+        } else {
+            res.redirect('/patient');
+        }
+        });
+    }
+
+    exports.get_deletePatient = function (req, res) {
+        Patient.findOneAndDelete({ _id: req.query.id }, function (err) {
+            if (err) {
+                // handle error
+                console.log(err);
+            } else {
+                res.redirect('/patient');
+            }
+        })
+    }
--- routes/patient.js
+++ routes/patient.js
@@ -7,4 +7,10 @@ router.get('/', patientController.get_patient);
 router.get('/addNewPatient', patientController.get_addNewPatient);
 
 router.post('/create', patientController.post_newPatient);
+
+router.get('/update', patientController.get_updatePatient);
+router.post('/update', patientController.post_updatePatient);
+
+router.get('/delete', patientController.get_deletePatient);
+
 module.exports = router;
