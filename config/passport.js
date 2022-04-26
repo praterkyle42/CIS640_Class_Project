08263d34ce2236fdeccb5e2f18fbef09b71ad0a5
praterkyle42 <kprater@gus.pittstate.edu>
2022-04-25 20:22:31 -0500

    fixed minor issues

--- config/passport.js
+++ config/passport.js
@@ -73,7 +73,6 @@ module.exports = function (passport) {
                 if (!user.validPassword(password))
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
                 // all is well, return successful user
-                console.log(user);
                 return done(null, user);
             });
         }));
--- controllers/accountDetailsController.js
+++ controllers/accountDetailsController.js
@@ -1,5 +1,4 @@
-const axios = require('axios');
-const User = require('../models/userSchema');
+let User = require('../models/userSchema');
 
 exports.getAccountDetails = function(req,res) {
     User.find({}, function(err, data) {
@@ -24,16 +23,13 @@ exports.post_newUser = function(req,res) {
     }
     
     let newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName, 
-        email: req.body.email, 
-        password: req.body.password, 
-        role: req.body.role
+        FirstName: req.body.firstName,
+        LastName: req.body.lastName, 
+        Email: req.body.email, 
+        Password: req.body.password, 
+        Role: req.body.role
     })
 
-    let temp = new User();
-    newUser.password = newUser.generateHash(req.body.password);
-    
     newUser.save(function(err) {
         if(err) {
             console.log(err)
@@ -47,12 +43,12 @@ exports.post_newUser = function(req,res) {
 }
 
 exports.get_updateUser = function(req,res) {
-    User.findOne({ _id: req.query.id }, function (err, User) {
+    User.findOne({ _id: req.query.id }, function (err, user) {
         if (err) {
             console.log(err);
         } else {
-            console.log(User);
-            res.render('AccountDetails/updateUser', { data: User });
+            console.log(user);
+            res.render('AccountDetails/updateUser', { data: user });
         }
     });
 
@@ -61,15 +57,13 @@ exports.get_updateUser = function(req,res) {
 exports.post_updateUser = function (req, res) {
 
     const updateData = {
-        FirstName: req.body.firstName,
-        LastName: req.body.lastName, 
-        Email: req.body.email, 
-        Password: req.body.password, 
-        Role: req.body.role
+        firstName: req.body.firstName,
+        lastName: req.body.lastName, 
+        email: req.body.email, 
+        password: req.body.password, 
+        role: req.body.role
     };
-    if (req.password) {
-    updateData.password = temp.generateHash(req.body.password)
-    }
+
     User.findOneAndUpdate({ _id: req.body.id }, updateData, function (err, data) {
         if (err) {
             // handle error
